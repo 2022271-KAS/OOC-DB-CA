55d133811156814f028a88001b4defac01f65479
Kevin S. <146198846+2022271-KAS@users.noreply.github.com>
2023-12-20 12:56:01 +0000

    Test

---
+++ src/ooc/db/ca/OOCDBCA.java
@@ -0,0 +1,18 @@
+package ooc.db.ca;
+
+/**
+ *
+ * @author Kevin
+ */
+public class OOCDBCA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Test");
+        
+    }
+    
+}
